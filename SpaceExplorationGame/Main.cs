49c338f0ebfc0ee4bc7a6012245489db7aa7b26f
Raven-V <veeennn.nn@gmail.com>
2024-11-30 06:13:47 +0800

    33% of the Program

--- SpaceExplorationGame/Main.cs
+++ SpaceExplorationGame/Main.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 using CharCreate;
 using CharacterStructure;
+using Stats;
 
 public class SpaceExplorationGame {
     public static void Main(string[] args) {
-        Console.WriteLine("-----Space Exploration Game-----");
-        Console.WriteLine("[1] New Game");
-        Console.WriteLine("[2] Load Game");
-        Console.WriteLine("[3] Credits");
-        Console.WriteLine("[4] Exit");
-        Console.Write("Choice: ");
+        while (true) {
+            Console.WriteLine("-----Space Exploration Game-----");
+            Console.WriteLine("[1] New Game");
+            Console.WriteLine("[2] Load Game");
+            Console.WriteLine("[3] Campaign");
+            Console.WriteLine("[4] Credits");
+            Console.WriteLine("[5] Exit");
+            Console.Write("Choice: ");
 
-        int choice = Convert.ToInt32(Console.ReadLine());
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-        switch (choice) {
-            case 1:
-                NewGame();
-                break;
-            case 3:
-                break;
-            case 4:
-                Environment.Exit(0); 
-                break;         
+            switch (choice) {
+                case 1:
+                    NewGame();
+                    break;
+                case 3:
+                    Campaign();
+                    break;
+                case 4:
+                    Credits();
+                    break;   
+                case 5:
+                    Environment.Exit(0); 
+                    break;         
+            }
         }
     }
 
@@ -36,7 +44,10 @@ public class SpaceExplorationGame {
         PrimaryWeapStruct Primary = Create.GetPrimary();
         SecondaryWeapStruct Secondary = Create.GetSecondary();
         MeleeStruct Melee = Create.GetMelee();
+        GrenadeStruct Grenade = Create.GetGrenade();
+        AllocateStatPoints CharStat = new AllocateStatPoints();
 
+        Create.Stats(CharStat);
         BasicAttri.Display();
         World.Display();
         Occupation.Display();
@@ -46,11 +57,101 @@ public class SpaceExplorationGame {
         Primary.Display();
         Secondary.Display();
         Melee.Display();
+        Grenade.Display();
+        CharStat.Display();
+
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Press any key to go back to main menu.");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+    public static void Campaign() {
+        Console.WriteLine("");
+        Console.WriteLine($"In a distant galaxy, a person embarked on a mission\n" + 
+            "to save their sibling, taken by a powerful organization obsessed with\n" + 
+            "controlling The Laugh Tail, a planet where no one had ever survived.\n" + 
+            "Legends spoke of its gates, sealed by six keys scattered across the\n" + 
+            "galaxy. The first stop was Ashara, a high-tech world of towering\n" + 
+            "cities and advanced AI. Navigating complex digital networks, the\n" + 
+            "person earned the trust of Ashara’s sentient AI and secured the first\n" + 
+            "key. With newfound cyber-skills, they set out, knowing greater challenges lay ahead.");
+
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"Next, they arrived on Pyros IX, a desert planet ruled by relentless\n" +
+            "sandstorms and fierce nomadic tribes. The tribes, who revered the second\n" +
+            "key as sacred, tested the person’s resolve through brutal survival trials.\n" +
+            "After mastering desert warfare and proving their strength, the person earned\n" +
+            "the tribes' respect and the key. On Vextris, a toxic wasteland plagued by acid\n" +
+            "rains and monstrous mutations, their endurance was tested further. Battling the\n" +
+            "harsh environment and mutated creatures, they unearthed the third key from the\n" +
+            "ruins of a forgotten civilization.");
+        
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"The journey then led to Auralis, a brutal prison planet where chaos\n" + 
+            "reigned and violence was law. To claim the fourth key, the person infiltrated\n" + 
+            "the prison hierarchy, forming uneasy alliances with the most dangerous inmates.\n" + 
+            "Enduring brutal combat and navigating deadly power struggles, they emerged\n" + 
+            "victorious, key in hand. From Auralis, they traveled to Noctivis, an opulent\n" + 
+            "orbital ring world where luxury hid corruption. Using diplomacy and deception,\n" + 
+            "they maneuvered through political intrigue, securing the fifth key through\n" + 
+            "cunning and wit.");
+
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);    
+        
+        Console.WriteLine("Their final stop before The Laugh Tail was Valtoria, the galaxy’s financial\n" + 
+            "hub, where wealth dictated power. The final key was locked in the vaults of\n" + 
+            "Valtoria’s most powerful factions, requiring both strategy and risk. In a daring\n" + 
+            "heist, the person outwitted the planet’s elite and secured the last key. With all\n" + 
+            "six keys now in their possession, they prepared for the ultimate challenge. They\n" + 
+            "knew the gates of The Laugh Tail would soon open, leading them to a fate no one\n" + 
+            "else had survived.");
+
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);    
+        
+        Console.WriteLine("Standing at the gates of The Laugh Tail, the person felt the weight of their\n" + 
+            "journey, but fear did not stop them. The fate of their sibling and the promise\n" + 
+            "of survival drove them forward, ready to face whatever waited beyond. They had\n" + 
+            "leveled up through every trial, mastering new skills and gaining allies in\n" + 
+            "unexpected places. With a deep breath, they unlocked the gates, stepping into\n" + 
+            "the unknown. What lay ahead would test everything they had become.");
+
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);    
+
+        Console.WriteLine("Press any key to go back to main menu.");
+        Console.ReadKey();
+        Console.Clear();
     }
 
     public static void Credits() {
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);
+
         Console.WriteLine("-----Raven Villanueva-----");
         Console.WriteLine("-----Lenardo Jualo------");
         Console.WriteLine("-----Daniel Gutierrez-------");
+
+        Console.WriteLine("");
+        Console.WriteLine("Loading....");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Press any key to go back to main menu.");
+        Console.ReadKey();
+        Console.Clear();
     }
 }
---
+++ SpaceExplorationGame/PointsAllocation.cs
@@ -0,0 +1,88 @@
+using System;
+
+namespace Stats {
+    public class AllocateStatPoints {
+        private int Strength { get; set; }
+        private int Dexterity { get; set; }
+        private int Constitution { get; set; }
+        private int Intelligence { get; set; }
+        private int Wisdom { get; set; }
+        private int Charisma { get; set; }
+        private int RemainingPoints { get; set; }
+        private const int MaxPoints = 10;
+        private const int AllocationPoints = 20;
+
+        public AllocateStatPoints() {
+            RemainingPoints = AllocationPoints;
+        }
+
+        public void AskStats() {
+            Console.WriteLine(" ");
+            Console.WriteLine($"You have {AllocationPoints} points to allocate across the following stats.");
+            Console.WriteLine($"Each stat have maximum points of {MaxPoints}.");
+           
+           Strength = GetStatsPoints("Strength");
+           if (RemainingPoints == 0) { AutoAllocate(); return; }
+
+           Dexterity = GetStatsPoints("Dexterity");
+           if (RemainingPoints == 0) { AutoAllocate(); return; }
+
+           Constitution = GetStatsPoints("Constitution");
+           if (RemainingPoints == 0) { AutoAllocate(); return; }
+
+           Intelligence = GetStatsPoints("Intelligence");
+           if (RemainingPoints == 0) { AutoAllocate(); return; }
+
+           Wisdom = GetStatsPoints("Wisdom");
+           if (RemainingPoints == 0) { AutoAllocate();return; }
+
+           Charisma = GetStatsPoints("Charisma");
+           if (RemainingPoints == 0) { AutoAllocate(); return; } 
+        }
+
+        public int GetStatsPoints(string  StatName) {
+            while (true) {
+                Console.WriteLine(" ");
+                Console.WriteLine($"Remaining Points {RemainingPoints}.");
+                Console.Write($"{StatName}: ");
+
+                try {
+                    int Points = Convert.ToInt32(Console.ReadLine());
+
+                    if (Points < 0) {
+                        throw new Exception("Points must be greater or equal than 0.");
+                    } else if (Points > MaxPoints) {
+                        throw new Exception("You cannot exceed over 10.");
+                    } else if (Points > RemainingPoints) {
+                        throw new Exception($"You only have {RemainingPoints} remaining.");
+                    } else {
+                        RemainingPoints -= Points;
+                        return Points;
+                    }
+                } catch (FormatException ) {
+                    Console.WriteLine("Number only!!");
+                } catch (Exception) {
+                    Console.WriteLine("Error!!");
+                }
+            }
+        }
+
+        public void AutoAllocate() {
+            if (Strength == 0) { Strength = 1;} 
+            if (Dexterity == 0) { Dexterity = 1;} 
+            if (Constitution == 0) { Constitution = 1; } 
+            if (Intelligence == 0) { Intelligence = 1; } 
+            if (Wisdom == 0) { Wisdom = 1; } 
+            if (Charisma == 0) { Charisma = 1; }
+        }
+
+        public void Display() {
+            Console.WriteLine($"Strength : {Strength}");
+            Console.WriteLine($"Dexterity : {Dexterity}");
+            Console.WriteLine($"Constitution : {Constitution}");
+            Console.WriteLine($"Intelligence : {Intelligence}");
+            Console.WriteLine($"Wisdom : {Wisdom}");
+            Console.WriteLine($"Charisma : {Charisma}");
+        }
+    }
+}
